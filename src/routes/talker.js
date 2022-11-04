32ee53b6eaebbc05922f4b9fd38028b2709b6901
Yury Stolarz <yurysscareer@gmail.com>
2022-11-04 15:05:00 -0300

    req 2 completo

--- src/routes/talker.js
+++ src/routes/talker.js
@@ -17,4 +17,22 @@ talker.get('/', async (req, res) => {
   }
 });
 
+talker.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await readFile();
+
+    const searchForPerson = data.find((person) => person.id === Number(id));
+    
+    if (searchForPerson) {
+      return res.status(200).json(searchForPerson);
+    }
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 module.exports = talker;
