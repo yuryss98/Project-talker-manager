584233d4454f996f040a5e2d226dd813844466cf
Yury Stolarz <yurysscareer@gmail.com>
2022-11-04 23:12:43 -0300

    atualiza rateMiddleware

--- src/middlewares/rateMiddleware.js
+++ src/middlewares/rateMiddleware.js
@@ -3,13 +3,7 @@ const ratingMiddleware = (req, res, next) => {
 
   const intervalNumber = rate >= 1 && rate <= 5;
 
-  if (!intervalNumber) {
-    return res.status(400).json(
-      { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
-    );
-  }
-
-  if (!Number.isInteger(rate)) {
+  if (!intervalNumber || !Number.isInteger(rate)) {
     return res.status(400).json(
       { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
     );
